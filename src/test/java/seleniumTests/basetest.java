60cdc78f7b2305d29fea158071e2ea1064a1a096
Lenovo <Lenovo@DESKTOP-KDFR93V>
2024-06-10 23:46:29 +0530

    ID18189/Commiting newTest

---
+++ src/test/java/seleniumTests/basetest.java
@@ -0,0 +1,12 @@
+package seleniumTests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class basetest {
+
+	public static void main(String[] args) {
+	WebDriver driver = new ChromeDriver();
+	}
+
+}
