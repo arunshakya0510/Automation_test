5baebd250c604a64757263de2ee109b4d46882e5
Lenovo <Lenovo@DESKTOP-KDFR93V>
2024-06-10 23:30:20 +0530

    ID1011/Commiting DemotestProject

---
+++ src/test/java/seleniumTests/FirstTest.java
@@ -0,0 +1,12 @@
+package seleniumTests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+	WebDriver driver = new ChromeDriver();
+	}
+
+}
---
+++ src/test/java/seleniumTests/SecondTest.java
@@ -0,0 +1,18 @@
+package seleniumTests;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SecondTest {
+
+	public static void main(String[] args) {
+	WebDriver driver = new ChromeDriver();
+	
+	
+
+	}
+
+}
